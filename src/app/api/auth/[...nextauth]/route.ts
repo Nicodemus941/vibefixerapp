88f6b07315b321e137300fe4a19ae4ee2200e836
Nic Baptiste <nicod@glowsuite.com>
2025-06-08 12:17:36 -0400

    chore: test after repo rename to vibefixerapp

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,7 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "./prisma";
 
 const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
