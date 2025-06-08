b2bed8c4f52e4a8d828fd86794264e3949d3f0ac
Nic Baptiste <nicod@glowsuite.com>
2025-06-08 12:26:57 -0400

    fix: move SessionProvider to client component to resolve Next.js server component error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
+import SessionProviderWrapper from "./SessionProviderWrapper";
 import AuthHeader from "./components/AuthHeader";
 
 const geistSans = Geist({
@@ -29,10 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionProvider>
+        <SessionProviderWrapper>
           <AuthHeader />
           {children}
-        </SessionProvider>
+        </SessionProviderWrapper>
       </body>
     </html>
   );
